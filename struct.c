767c4c91c7f61cf1c49d62a7c21e14f7b2deb272
Miki Pokryvailo <dpokryvailo@cs.stonybrook.edu>
2017-09-26 22:40:21 -0400

    First commit

---
+++ struct.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+typedef struct {
+	int attempts;
+	char *password;
+	char *errorMessage;
+	int maxAttempts;
+} login;
+
+void tryLogin(login *login, char *password) {
+	if (strcmp(login->password, password) == 0) {
+		printf("%s", "Yay, you've logged in!");
+	} else {
+		printf("%s", login->errorMessage);
+		 login->attempts++;
+	}
+}
+
+int main( int argc, const char* argv[] )
+{
+login attempt = {1, "Michelle", "That's not the password, you silly goose!", 3};
+tryLogin(&attempt, "Michelle");
+
+
+	
+}
+
